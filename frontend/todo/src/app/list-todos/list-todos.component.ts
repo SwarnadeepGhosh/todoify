521663209dbea1d6d6d6a884cc089dfc079fc7f2
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2022-05-13 22:58:55 +0530

    Frontend- Added READ and DELETE features

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../services/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -15,13 +16,33 @@ export class Todo {
   styleUrls: ['./list-todos.component.css'],
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, 'My first Todo', true, new Date()),
-    new Todo(1, 'Welcome to India', false, new Date()),
-    new Todo(1, 'Angular project', true, new Date()),
-  ];
+  message: string | undefined;
+  todos: Todo[] = [];
+  // [
+  //   new Todo(1, 'My first Todo', true, new Date()),
+  //   new Todo(1, 'Welcome to India', false, new Date()),
+  //   new Todo(1, 'Angular project', true, new Date()),
+  // ];
 
-  constructor() {}
+  constructor(private todoService: TodoDataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.refreshTodos();
+  }
+
+  refreshTodos(){
+    this.todoService.getAllTodos('user').subscribe((response) => {
+      // console.log(response);
+      this.todos = response;
+    });
+  }
+
+  deleteTodo(id: number) {
+    console.log(`Todo Deleted ${id}`);
+    this.todoService.deleteTodo('user', id).subscribe((response) => {
+      console.log(response);
+      this.message = `Delete of Todo ${id} successful`;
+      this.refreshTodos();
+    });
+  }
 }
