addb02c8ee29207c4c3a2a35adac0b3cf927ecc9
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2023-07-04 20:55:39 +0530

    backend moved to docker

--- frontend/todo/src/environments/environment.prod.ts
+++ frontend/todo/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://todo-fullstack-backend.herokuapp.com' // baseUrl is the URL for backend
+  baseUrl: 'https://todoify-backend.azurewebsites.net' // baseUrl is the URL for backend
 };
